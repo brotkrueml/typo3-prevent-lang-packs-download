faa2358b6b2ce81565497bd16e19a917c20a4b4c
Chris Müller <coding@brotkrueml.dev>
2025-11-28 19:08:23 +0100

    Add compatibility for TYPO3 v14

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+- Compatibility with TYPO3 v14
+
 ## [1.0.0] - 2024-11-21
 
 Initial release
--- ext_emconf.php
+++ ext_emconf.php
@@ -9,7 +9,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.1.0-dev',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-13.4.99',
+            'typo3' => '12.4.0-14.3.99',
         ],
         'conflicts' => [],
         'suggests' => [],
